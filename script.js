b986eff8abeb0728621b30636e5cf7bb3b27c839
leosum1t <pokharelsumit001@gmail.com>
2026-07-06 13:31:07 +0545

    fixed some issues in operator

--- script.js
+++ script.js
@@ -13,26 +13,50 @@ function updateDisplay(value) {
     display.textContent = displayValue;
 
     display.classList.remove("text-6xl", "text-4xl", "text-3xl", "text-2xl");
-    if (displayValue.length > 10) {
-        display.classList.add("text-3xl");
-    } else if (displayValue.length > 20) {
+    if (displayValue.length > 20) {
         display.classList.add("text-2xl");
+    } else if (displayValue.length > 10) {
+        display.classList.add("text-3xl");
     } else {
         display.classList.add("text-6xl");
     }
-
     display.scrollLeft = display.scrollWidth;
 }
 
 buttons.forEach(button => {
     button.addEventListener("click", () => {
         const value = button.dataset.value;
+        const operators = ["+", "-", "*", "/"];
+
+        if (value === ".") {
+            const lastOperator = Math.max(
+                expression.lastIndexOf("+"),
+                expression.lastIndexOf("-"),
+                expression.lastIndexOf("*"),
+                expression.lastIndexOf("/")
+            );
+
+            const currentNumber = expression.slice(lastOperator + 1);
+
+            if (currentNumber.includes(".")) {
+                return;
+            }
+        }
         if (value === "%") {
             expression = (Number(expression) / 100).toString();
             updateDisplay(expression);
             return;
         }
 
+        if (operators.includes(value)) {
+            const lastChar = expression.slice(-1);
+
+            if (operators.includes(lastChar)) {
+                expression = expression.slice(0, -1) + value;
+                updateDisplay(expression);
+                return;
+            }
+        }
         if (display.textContent === "0" && value !== ".") {
             expression = value;
         } else {
